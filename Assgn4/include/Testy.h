ff4f44d313313cf83918411b4ef42aa575ba7455
rahul <rahul@ubuntu>
2012-11-24 13:24:51 -0500

    added .h files for my code

---
+++ Assgn4/include/Testy.h
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//returns the size of the file
+int getSize(char* filename);
+
+void readFile(char* s, char* source);
+
+void writeFile(char* data, int size, char* destination);
